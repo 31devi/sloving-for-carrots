7d9c516d2f704ef142b51743ce7e814a13928ee5
31devi <77260949+31devi@users.noreply.github.com>
2021-01-19 15:55:00 +0530

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+  public static void main(String[]args){
+    Scanner kbd= new Scanner(System.in);
+     int n= kbd.nextInt();
+     int p=kbd.nextInt();
+     System.out.print(p);
+  }
+}
---
+++ carrots.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n,p;
+   cin>>n;
+   cin>>p;
+  if (1<=n && p<=1000){
+    cout<<p;
+  }
+  
+
+}
